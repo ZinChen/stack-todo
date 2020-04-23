339eb7731568b97132a28429ec9746da21e5024a
Yuri Zinchenko <zinchen777@gmail.com>
2020-04-23 17:36:43 +0300

    fix apk script

--- create_apk_and_install.sh
+++ create_apk_and_install.sh
@@ -1,7 +1,8 @@
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore dist/my-release-key.keystore dist/cordova/android/apk/release/app-release-unsigned.apk alias_name
-./dist/zipalign -v 4 /dist/cordova/android/apk/release/app-release-unsigned.apk StackTest.apk
+rm dist/StackTest.apk
+./dist/zipalign -v 4 dist/cordova/android/apk/release/app-release-unsigned.apk dist/StackTest.apk
 
 read -p "Do you want to install apk via adb? (y or n)" yn
 case ${yn:-N} in
-  [Yy]* ) adb install StackTest.apk;
+  [Yy]* ) adb install dist/StackTest.apk;
 esac
